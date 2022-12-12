c4a22cc41020ecb45420f4c3071abfd3b7cbd645
brandel-T <tsagueubrandel@gmail.com>
2022-12-12 05:45:30 +0100

    adding the menu page

---
+++ src/views/MenuOverview/MenuOverview.service.ts
@@ -0,0 +1,12 @@
+import { reactive } from "vue"
+
+const useMenuOverviewService = () => {
+
+    const state = reactive({ })
+
+    return {
+        state
+    }
+}
+
+export default useMenuOverviewService;
---
+++ src/views/MenuOverview/MenuOverview.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>menu overview works</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "MenuOverview",
+    components: {},
+    setup() {
+
+        return {}
+    }
+})
+</script>
+
+<style lang="scss" scoped>
+</style>
